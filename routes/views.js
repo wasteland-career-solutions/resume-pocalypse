3f3821deefe2b0a5b97b899a1c54ba93bdce601c
Jose Segura <jossegura2023@u.northwestern.edu>
2023-05-05 11:53:35 -0500

    fixed route direct to homepage when server starts

---
+++ routes/views.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.render('main');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+module.exports = router;
