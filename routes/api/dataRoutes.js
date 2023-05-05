314d0015b286d927eb4c880af9ad391a782548ba
Reid M <reidmadock@gmail.com>
2023-05-04 20:43:19 -0500

    changed false data condition n dataroute

--- routes/api/dataRoutes.js
+++ routes/api/dataRoutes.js
@@ -8,11 +8,12 @@ router.get('/UserData', async (req, res) => {
             res.status(401).json({message: 'You must be logged in to call this function.'});
             return;    
         }
-        
+
         const data = await UserData.findAll({ where: {id: req.session.user.id } });
 
         if (!data) {
             res.status(401).json({message: 'Sorry! No data was found associated with your session.'});
+            res.redirect('/data'); // Redirect user to additional data page.
             return;
         }
 
