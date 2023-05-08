40165d96a04d98b0493030490b5c3fe69463537b
Reid M <reidmadock@gmail.com>
2023-05-08 16:34:05 -0500

    not working fully but whatever at least it loads questions

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -16,25 +16,34 @@ router.get('/login', (req, res) => {
     res.render('login', { logged_in: req.session.logged_in, });
 });
 
-router.get('/game', async (req, res) => {
-    try{
-        const dbQuestionData = Question.findAll();
+// router.get('/game', async (req, res) => {
+//     try{
+//         const dbQuestionData = Question.findAll();
 
-        const questions = (await dbQuestionData).map((question) =>
-            question.get({ plain: true })
-        );
+//         const questions = (await dbQuestionData).map((question) =>
+//             question.get({ plain: true })
+//         );
 
-        if (req.session.logged_in) {
-            res.render('game', { questions, logged_in: req.session.logged_in });
-        }
-        else {
-            res.redirect('login');
-            alert('Please log in to play the game.')
-        }
-    } catch (err) {
-        console.error(err);
-    }
+//         if (req.session.logged_in) {
+//             res.render('game', { questions, logged_in: req.session.logged_in });
+//         }
+//         else {
+//             res.redirect('login');
+//             alert('Please log in to play the game.')
+//         }
+//     } catch (err) {
+//         console.error(err);
+//     }
     
+// });
+
+router.get('/game', async (req, res) => {
+    if (req.session.logged_in) {
+        res.render('game', { logged_in: req.session.logged_in });
+    } else {
+        res.redirect('login');
+        alert('Please log in to play the game.');
+    }
 });
 
 module.exports = router;
--- public/js/gamelogic.js
+++ public/js/gamelogic.js
@@ -1,8 +1,9 @@
 let position = 0;
 const container = document.querySelector('#game-container');
 const answerField = document.querySelector('#answer-box');
+const answerForm = document.querySelector('#answer-form');
 const qAnsList = document.querySelector('#q-and-a-list');
-const allQuestionArr = fetchQuestions();
+let allQuestionArr = fetchQuestions();
 const userAnswers = [];
 
 async function fetchQuestions() {
@@ -11,7 +12,7 @@ async function fetchQuestions() {
             method: 'GET',
             headers: { 'Content-Type': 'application/json' },
         });
-        const data = await result.json;
+        const data = await result.json();
         return data;
     } catch (err) {
         console.error(err);
@@ -32,12 +33,15 @@ function loadQuestion() {
 
     newQ.setAttribute('class', 'question-container');
 
-    if(answerField.textContent) { // Check if there was an answer to store in the list item entry
-        userAnswer.textContent = answerField.textContent
+    if(answerField.value) { // Check if there was an answer to store in the list item entry
+        userAnswer.textContent = answerField.value;
         newLi.appendChild(userAnswer);
+    } else {
+        console.log('Error storing response');
     }
     
     newLi.appendChild(newQ); // Store the previous question
+    newLi.appendChild(userAnswer);
     qAnsList.appendChild(newLi);
 }
 
@@ -46,13 +50,23 @@ function renderNextQuestion() {
     
 }
 
-fetchQuestions();
+async function init() {
+    try{
+        allQuestionArr = await fetchQuestions();
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+
+init(); 
 
 document.querySelector('#next-button').addEventListener('click', (event) => {
     event.preventDefault();
-    if(answerField.textContent) {
-        userAnswers.push(answerField.textContent); // Store the answer
+    if(answerField.value != "" && answerField.value != answerField.defaultValue) {
+        userAnswers.push(answerField.value); // Store the answer
         answerField.textContent = ""; // Clear the answer box
+        document.querySelector('#answer-form').reset();
     } else {
         return; // do nothing if the user has not answered a question
     }
