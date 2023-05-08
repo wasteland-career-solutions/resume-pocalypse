01e600cfebec5c4332e9e02ce34e4e324b551421
Reid M <reidmadock@gmail.com>
2023-05-08 15:52:26 -0500

    promising solution, not yet implemented fully

--- public/js/gamelogic.js
+++ public/js/gamelogic.js
@@ -1,10 +1,13 @@
-const Handlebars = import('express-handlebars');
+let position = 0;
+const container = document.querySelector('#game-container');
+const answerField = document.querySelector('#answer-box');
+const qAnsList = document.querySelector('#q-and-a-list');
+const allQuestionArr = fetchQuestions();
+const userAnswers = [];
 
-let position = 1;
-
-async function fetchQuestion(pos) {
+async function fetchQuestions() {
     try {
-        const result = await fetch(`/api/users/play:${pos}`, {
+        const result = await fetch(`/api/users/play`, {
             method: 'GET',
             headers: { 'Content-Type': 'application/json' },
         });
@@ -17,35 +20,42 @@ async function fetchQuestion(pos) {
 
 function nextQuestion() {
     position++;
-    renderQuestion();
 }
 
-function renderQuestion() {
-    const container = document.querySelector('#game-container');
-    const hasNext = false;
-    const question = fetchQuestion(position);
+function loadQuestion() {
+    const newLi = document.createElement('li')
+    const newQ = document.createElement('h2')
+    const userAnswer = document.createElement('p');
 
-    container.innerHTML = `<div id="game-container">
-    <h2>Question ${question.id}:</h2>
-    <p>Value: ${question.body}</p>
-    {{#if ${hasNext} }}
-        <button class="btn btn-primary" id="next-button">Next</button>
-    {{/if}}
-</div>`
+    newQ.textContent = allQuestionArr[position].body;
+    nextQuestion();
 
-    if(hasNext) {
-        nextQuestion();
+    newQ.setAttribute('class', 'question-container');
+
+    if(answerField.textContent) { // Check if there was an answer to store in the list item entry
+        userAnswer.textContent = answerField.textContent
+        newLi.appendChild(userAnswer);
     }
+    
+    newLi.appendChild(newQ); // Store the previous question
+    qAnsList.appendChild(newLi);
 }
 
-function init() {
-    renderQuestion();
+function renderNextQuestion() {
+    
+    
 }
 
-init();
+fetchQuestions();
 
-document.querySelector('#next-button')
-        .addEventListener('click', (event) => {
-            event.preventDefault();
-        nextQuestion();
+document.querySelector('#next-button').addEventListener('click', (event) => {
+    event.preventDefault();
+    if(answerField.textContent) {
+        userAnswers.push(answerField.textContent); // Store the answer
+        answerField.textContent = ""; // Clear the answer box
+    } else {
+        return; // do nothing if the user has not answered a question
+    }
+
+    loadQuestion();
 });
