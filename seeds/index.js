5287e3686913e7c7f68f9347a1b30c17217de319
AnthGrow <122587083+AnthGrow@users.noreply.github.com>
2023-05-08 22:18:12 -0500

    added seed data for user and userData

--- seeds/index.js
+++ seeds/index.js
@@ -1,17 +1,25 @@
 const sequelize = require('../config/connection');
-const { Question } = require('../models');
+const { Question, Answer, User, UserData } = require('../models');
 // const Question = require('../models/Question');
 
 const questionSeedData = require('./questionSeedData.json');
 
 const answerSeedData = require('./answerSeedData.json');
 
+const userSeedData = require("./userSeedData.json");
+
+const userDataSeedData = require("./userDataSeedData.json");
+
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
 
     const questions = await Question.bulkCreate(questionSeedData);
 
-    const answers = await Answer.bulkCreate(answerSeedData.json);
+    const answers = await Answer.bulkCreate(answerSeedData);
+
+    const user = await User.bulkCreate(userSeedData);
+
+    const userData = await UserData.bulkCreate(userDataSeedData);
 
     // await Question.bulkCreate(questionSeedData, {
     //     individualHooks: true,
